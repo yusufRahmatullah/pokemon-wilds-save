2f5ca28241145cc82a3002a3a5c745100428e1c2
Yusuf Rahmatullah <yusuf.rahmatullah@bukalapak.com>
2022-03-04 23:47:29 +0700

    Add dummy backup for duplicate main island

---
+++ copy_dummy.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+
+DUMMY_DIR = 'dummy_backup'
+MAP_DIR = 'small.sav'
+DEFAULT_DUMMY = '-1,0'
+
+
+def validate_input(x, y):
+    fname = f'spawnnull{x},{y}'
+    if os.path.exists(os.path.join(MAP_DIR, fname)):
+        raise ValueError(f'{fname} already exist')
+
+
+def copy_file(src, dest):
+    src_path = os.path.join(DUMMY_DIR, src)
+    dest_path = os.path.join(MAP_DIR, dest)
+
+    os.system(f'cp "{src_path}" "{dest_path}"')
+
+
+def copy_files(x, y):
+    os_name = f'spawnnull{DEFAULT_DUMMY}'
+    oj_name = f'map{DEFAULT_DUMMY}.png'
+    om_name = f'map{DEFAULT_DUMMY}'
+
+    s_name = f'spawnnull{x},{y}'
+    j_name = f'map{x},{y}.png'
+    m_name = f'map{x},{y}'
+
+    copy_file(os_name, s_name)
+    copy_file(oj_name, j_name)
+    copy_file(om_name, m_name)
+
+
+def main():
+    args = sys.argv
+    if len(args) != 3:
+        print(f'Usage: python3 {args[0]} <x> <y>')
+        sys.exit(1)
+
+    x = args[1]
+    y = args[2]
+    print(f'Your input x: {x}, y: {y}')
+
+    validate_input(x, y)
+    copy_files(x, y)
+
+
+if __name__ == '__main__':
+    main()
